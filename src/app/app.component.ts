68663d3073192daaf7e48c189541d001bc12112e
Birkir Brynjarsson <birkir@birkir.is>
2026-04-13 12:01:40 +0000

    Update Angular from v4 to v20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,81 +1,70 @@
-import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
-
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
+import firebase from 'firebase/compat/app';
 
 @Component({
+  standalone: false,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @ViewChild('myTable') table: any;
-  @ViewChild('dateHdr') dateHdr: TemplateRef<any>;
-  @ViewChild('idHdr') idHdr: TemplateRef<any>;
-  @ViewChild('objectHdr') objectHdr: TemplateRef<any>;
-  @ViewChild('descHdr') descHdr: TemplateRef<any>;
-  @ViewChild('xHdr') xHdr: TemplateRef<any>;
-  @ViewChild('yHdr') yHdr: TemplateRef<any>;
-  @ViewChild('altHdr') altHdr: TemplateRef<any>;
-  @ViewChild('zoneHdr') zoneHdr: TemplateRef<any>;
-  @ViewChild('layerHdr') layerHdr: TemplateRef<any>;
-  @ViewChild('regHdr') regHdr: TemplateRef<any>;
+  @ViewChild('dateHdr') dateHdr!: TemplateRef<any>;
+  @ViewChild('idHdr') idHdr!: TemplateRef<any>;
+  @ViewChild('objectHdr') objectHdr!: TemplateRef<any>;
+  @ViewChild('descHdr') descHdr!: TemplateRef<any>;
+  @ViewChild('xHdr') xHdr!: TemplateRef<any>;
+  @ViewChild('yHdr') yHdr!: TemplateRef<any>;
+  @ViewChild('altHdr') altHdr!: TemplateRef<any>;
+  @ViewChild('zoneHdr') zoneHdr!: TemplateRef<any>;
+  @ViewChild('layerHdr') layerHdr!: TemplateRef<any>;
+  @ViewChild('regHdr') regHdr!: TemplateRef<any>;
 
-  rows = [];
-  temp = [];
-  columns = [];
+  rows: any[] = [];
+  temp: any[] = [];
+  columns: any[] = [];
   expanded: any = {};
-  timeout: any;
+  timeout?: ReturnType<typeof setTimeout>;
 
   // FilterInputs
   filters = {
-    all: "",
-    date: "",
-    ID: "",
-    object: "",
-    description: "",
-    analysis: "",
-    x: "",
-    y: "",
-    altitude: "",
-    zone: "",
-    mineralLayer: "",
-    registration: "",
-    registrationID: "",
-    storage: ""
+    all: '',
+    date: '',
+    ID: '',
+    object: '',
+    description: '',
+    analysis: '',
+    x: '',
+    y: '',
+    altitude: '',
+    zone: '',
+    mineralLayer: '',
+    registration: '',
+    registrationID: '',
+    storage: ''
   };
-  
 
+  user: Observable<firebase.User | null>;
+  private readonly itemsRef: AngularFireList<any>;
+  msgVal = '';
 
-  user : Observable<firebase.User>;
-  data : FirebaseListObservable<any[]>;
-  msgVal : string = '';
-  filter : any = { registration: '' };
-
-  public filterQuery : string = "";
-  public rowsOnPage : number = 10;
-  public sortBy : string = "email";
-  public sortOrder : string = "asc";
-
-  constructor(public afAuth : AngularFireAuth, public af : AngularFireDatabase) {
-    this.data = af.list('/items');
-    this.data.subscribe(snapshot => {
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.itemsRef = af.list('/items');
+    this.itemsRef.valueChanges().subscribe(snapshot => {
       this.temp = [...snapshot];
       this.rows = snapshot;
     });
-    // console.log(this.items);
-
     this.user = this.afAuth.authState;
-
   }
 
-  ngOnInit(){
+  ngOnInit(): void {
     this.columns = [
       { headerTemplate: this.dateHdr, name: 'Dagsetning', prop: 'date', width: 95, canAutoResize: false },
-      { headerTemplate: this.idHdr, name: '# ID', prop: 'ID', width: 100, canAutoResize: false }, 
+      { headerTemplate: this.idHdr, name: '# ID', prop: 'ID', width: 100, canAutoResize: false },
       { headerTemplate: this.objectHdr, name: 'Munur', prop: 'object', minWidth: 100, canAutoResize: true },
       { headerTemplate: this.descHdr, name: 'Lýsing', prop: 'description', minWidth: 100, canAutoResize: true },
       { headerTemplate: this.xHdr, prop: 'x', width: 60, canAutoResize: false },
@@ -87,90 +76,100 @@ export class AppComponent {
     ];
   }
 
-  onPage(event) {
-    clearTimeout(this.timeout);
+  onPage(event: any): void {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
     this.timeout = setTimeout(() => {
-      console.log('paged!', event);
+      // console.log('paged!', event);
     }, 100);
   }
 
-  onActivate(event) {
-    console.log(event);
-    if(event.type === 'click' || event.event.key === "Enter"){
+  onActivate(event: any): void {
+    // console.log(event);
+    if (event.type === 'click' || event.event?.key === 'Enter') {
       this.table.rowDetail.toggleExpandRow(event.row);
-    } 
+    }
   }
 
-  toggleExpandRow(row) {
-    console.log('Toggled Expand Row!', row);
+  toggleExpandRow(row: any): void {
+    // console.log('Toggled Expand Row!', row);
     this.table.rowDetail.toggleExpandRow(row);
   }
 
-  onDetailToggle(event) {
+  onDetailToggle(event: any): void {
     console.log('Detail Toggled', event);
   }
 
-  filterText(event){
-    for(var key in this.filters){
-      this.filters[key] = this.filters[key].toLowerCase();
+  private fieldContains(data: any, field: string, filterValue: string): boolean {
+    if (!filterValue) {
+      return true;
     }
-    // console.log(this.filters);
-    const temp = this.temp.filter((data) => {
-      return (!this.filters['date'] || data['date'].toLowerCase().indexOf(this.filters['date']) !== -1)
-      && (!this.filters['ID'] || data['ID'].toLowerCase().indexOf(this.filters['ID']) !== -1)
-      && (!this.filters['object'] || data['object'].toLowerCase().indexOf(this.filters['object']) !== -1)
-      && (!this.filters['description'] || data['description'].toLowerCase().indexOf(this.filters['description']) !== -1)
-      && (!this.filters['analysis'] || data['analysis'].toLowerCase().indexOf(this.filters['analysis']) !== -1)
-      && (!this.filters['x'] || data['x'].toLowerCase().indexOf(this.filters['x']) !== -1)
-      && (!this.filters['y'] || data['y'].toLowerCase().indexOf(this.filters['y']) !== -1)
-      && (!this.filters['altitude'] || data['altitude'].toLowerCase().indexOf(this.filters['altitude']) !== -1)
-      && (!this.filters['zone'] || data['zone'].toLowerCase().indexOf(this.filters['zone']) !== -1)
-      && (!this.filters['mineralLayer'] || data['mineralLayer'].toLowerCase().indexOf(this.filters['mineralLayer']) !== -1)
-      && (!this.filters['registrationID'] || data['registration'].toLowerCase().indexOf(this.filters['registrationID']) !== -1
-      || !this.filters['registrationID'] || data['registrationID'].toLowerCase().indexOf(this.filters['registrationID']) !== -1)
-      && (!this.filters['storage'] || data['storage'].toLowerCase().indexOf(this.filters['storage']) !== -1)
-      && (!this.filters['all'] 
-      || data['date'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['ID'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['object'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['description'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['analysis'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['x'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['y'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['altitude'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['zone'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['mineralLayer'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['registration'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['registrationID'].toLowerCase().indexOf(this.filters['all']) !== -1
-      || data['storage'].toLowerCase().indexOf(this.filters['all']) !== -1)
+    const value = String(data?.[field] ?? '').toLowerCase();
+    return value.indexOf(filterValue) !== -1;
+  }
+
+  filterText(_event: Event): void {
+    for (const key of Object.keys(this.filters)) {
+      const current = this.filters[key as keyof typeof this.filters];
+      this.filters[key as keyof typeof this.filters] = current.toLowerCase();
+    }
+
+    const filtered = this.temp.filter((data: any) => {
+      const regFilterMatch =
+        !this.filters.registrationID ||
+        this.fieldContains(data, 'registration', this.filters.registrationID) ||
+        this.fieldContains(data, 'registrationID', this.filters.registrationID);
+
+      return this.fieldContains(data, 'date', this.filters.date)
+      && this.fieldContains(data, 'ID', this.filters.ID)
+      && this.fieldContains(data, 'object', this.filters.object)
+      && this.fieldContains(data, 'description', this.filters.description)
+      && this.fieldContains(data, 'analysis', this.filters.analysis)
+      && this.fieldContains(data, 'x', this.filters.x)
+      && this.fieldContains(data, 'y', this.filters.y)
+      && this.fieldContains(data, 'altitude', this.filters.altitude)
+      && this.fieldContains(data, 'zone', this.filters.zone)
+      && this.fieldContains(data, 'mineralLayer', this.filters.mineralLayer)
+      && regFilterMatch
+      && this.fieldContains(data, 'storage', this.filters.storage)
+      && (
+        !this.filters.all
+        || this.fieldContains(data, 'date', this.filters.all)
+        || this.fieldContains(data, 'ID', this.filters.all)
+        || this.fieldContains(data, 'object', this.filters.all)
+        || this.fieldContains(data, 'description', this.filters.all)
+        || this.fieldContains(data, 'analysis', this.filters.all)
+        || this.fieldContains(data, 'x', this.filters.all)
+        || this.fieldContains(data, 'y', this.filters.all)
+        || this.fieldContains(data, 'altitude', this.filters.all)
+        || this.fieldContains(data, 'zone', this.filters.all)
+        || this.fieldContains(data, 'mineralLayer', this.filters.all)
+        || this.fieldContains(data, 'registration', this.filters.all)
+        || this.fieldContains(data, 'registrationID', this.filters.all)
+        || this.fieldContains(data, 'storage', this.filters.all)
+      );
     });
 
-    // filter our data
-    // const temp = this.temp.filter(function(d) {
-    //   return d[prop].toLowerCase().indexOf(val) !== -1 || !val;
-    // });
-
-    // update the rows
-    this.rows = temp;
-    // Whenever the filter changes, always go back to the first page
+    this.rows = filtered;
     this.table.offset = 0;
   }
 
-  login(){
-    this.afAuth.auth.signInAnonymously();
+  login(): void {
+    this.afAuth.signInAnonymously();
   }
 
-  logout(){
-    this.afAuth.auth.signOut();
+  logout(): void {
+    this.afAuth.signOut();
   }
 
-  sendMessage(text) {
-    var message = {
-      message : text,
-      displayName : "Birkir",
-      email : "birkir@birkir.is",
+  sendMessage(text: string): void {
+    const message = {
+      message: text,
+      displayName: 'Birkir',
+      email: 'birkir@birkir.is',
       timestamp: Date.now()
-    }
-    this.data.push(message);
+    };
+    this.itemsRef.push(message);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-
-import { Ng2FilterPipeModule } from 'ng2-filter-pipe';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAr9kKXcDQtMAJINa5P1DshL2aVvIywcCo",
-  authDomain: "skrida-crud.firebaseapp.com",
-  databaseURL: "https://skrida-crud.firebaseio.com",
-  projectId: "skrida-crud",
-  storageBucket: "skrida-crud.appspot.com",
-  messagingSenderId: "784239624873"
+  apiKey: 'AIzaSyAr9kKXcDQtMAJINa5P1DshL2aVvIywcCo',
+  authDomain: 'skrida-crud.firebaseapp.com',
+  databaseURL: 'https://skrida-crud.firebaseio.com',
+  projectId: 'skrida-crud',
+  storageBucket: 'skrida-crud.appspot.com',
+  messagingSenderId: '784239624873'
 };
 
 @NgModule({
@@ -29,11 +26,9 @@ export const firebaseConfig = {
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
-    Ng2FilterPipeModule,
     NgxDatatableModule
   ],
   providers: [],
